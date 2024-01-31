8099c2e93d05e4ddf1a3a00fd3b9ed5a70bce4dc
HYUKSKEE <gin280833@gmail.com>
2024-01-31 17:53:27 +0900

    add: wayToCome 컴포넌트 추가

--- src/components/sections/Map.tsx
+++ src/components/sections/Map.tsx
@@ -18,6 +18,7 @@ function Map({ location }: { location: Location }) {
   useEffect(() => {
     const script = document.createElement('script')
     script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_APP_KEY}&autoload=false`
+    //비동기로 불러오기
     script.async = true
 
     document.head.appendChild(script)
@@ -44,11 +45,51 @@ function Map({ location }: { location: Location }) {
   }, [location])
 
   return (
-    <Section>
+    <Section
+      title={
+        <div className={cx('wrap-header')}>
+          <span className={cx('text-title')}>오시는길</span>
+          <span className={cx('text-subtitle')}>{location.name}</span>
+          <span className={cx('text-subtitle')}>{location.address}</span>
+        </div>
+      }
+    >
       <div className={cx('wrap-map')}>
         <div ref={mapContainer} className={cx('map')}></div>
+
+        <a
+          className={cx('button-find-way')}
+          href={location.link}
+          target="_blank"
+          rel="noreferrer"
+        >
+          길찾기
+        </a>
+      </div>
+      <div>
+        <WayToCome label="버스" list={location.waytocome.bus} />
+        <WayToCome label="지하철" list={location.waytocome.metro} />
       </div>
     </Section>
+  )
+}
+
+function WayToCome({
+  label,
+  list,
+}: {
+  label: React.ReactNode
+  list: string[]
+}) {
+  return (
+    <div className={cx('wrap-way-to-come')}>
+      <div className={cx('text-label')}>{label}</div>
+      <ul>
+        {list.map((WayToCome) => (
+          <li>{WayToCome}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
