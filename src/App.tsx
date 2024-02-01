176d5ea06c5ffbe51469042cbe2d9ad89bedaefb
HYUKSKEE <gin280833@gmail.com>
2024-02-01 17:26:30 +0900

    add : Dimmed 컴포넌트 추가, modal 컴포넌트 작업중

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Calendar from './components/sections/Calendar'
 import Map from './components/sections/Map'
 import Contact from './components/sections/Contact'
 import Share from './components/sections/Share'
+import Modal from './components/shared/Modal'
 
 const cx = classNames.bind(styles)
 
@@ -83,6 +84,24 @@ function App() {
       <Map location={location} />
       <Contact groom={groom} bride={bride} />
       <Share groomName={groom.name} brideName={bride.name} date={date} />
+
+      {/* <Modal
+        open={true}
+        title="현재 참석자"
+        body={
+          <div>
+            <input />
+          </div>
+        }
+        leftButtonLabel="취소"
+        rightButtonLabel="저장하기"
+        onLeftButtonClick={() => {
+          console.log('왼쪽 버튼 클릭')
+        }}
+        onRightButtonClick={() => {
+          console.log('오른쪽 버튼 클릭')
+        }}
+      /> */}
     </div>
   )
 }
