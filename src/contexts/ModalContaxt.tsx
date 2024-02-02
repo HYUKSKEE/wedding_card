b9f15fcec8ec8f05acf62261daf0a027fc0c8e05
HYUKSKEE <gin280833@gmail.com>
2024-02-02 19:11:55 +0900

    wip: modal context 작업중

---
+++ src/contexts/ModalContaxt.tsx
@@ -0,0 +1,50 @@
+import { createContext, useContext, ComponentProps, useState } from 'react'
+
+import Modal from '../components/shared/Modal'
+import { createPortal } from 'react-dom'
+
+type ModalProps = ComponentProps<typeof Modal>
+
+type ModalOptions = Omit<ModalProps, 'open'>
+
+interface ModalContextValue {
+  open: (oprions: ModalProps) => void
+  close: () => void
+}
+
+const Context = createContext<ModalContextValue | undefined>(undefined)
+
+const defaultValues: ModalProps = {
+  open: false,
+  body: null,
+  onRightButtonClick: () => {},
+  onLeftButtonClick: () => {},
+}
+
+function ModalContext({ children }: { children: React.ReactNode }) {
+  const [modalState, setModalState] = useState<ModalProps>(defaultValues)
+
+  const $portal_root = document.getElementById('root_portal')
+
+  const open = (options: ModalOptions) => {
+    setModalState({ ...options, open: true })
+  }
+
+  const close = () => {
+    setModalState(defaultValues)
+  }
+
+  const values = {
+    open,
+    close,
+  }
+
+  return (
+    <Context.Provider value={values}>
+      {children}
+      {$portal_root != null
+        ? createPortal(<Modal {...modalState} />, $portal_root)
+        : null}
+    </Context.Provider>
+  )
+}
